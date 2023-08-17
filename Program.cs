fb115ef33a77f56ab426833b2c86f42044b94ada
tun784 <thudientu782004@gmail.com>
2023-08-17 11:40:54 +0700

    August 17th

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Bai1 {
+    class program {
+        static void Main() {
+            int a, b;
+            Console.WriteLine("Nhap hai so nguyen bat ky.");
+            Console.Write("Moi nhap so thu nhat: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Moi nhap so thu hai: ");
+            b = int.Parse(Console.ReadLine());
+            while (b == 0) {
+                Console.Write("Ban da nhap so thu hai la so {0}, yeu cau ban nhap lai: ", b);
+                b = int.Parse(Console.ReadLine());
+            }
+            int tong = a + b;
+            int hieu = a - b;
+            int tich = a * b;
+            float thuong = (float) a / b;
+            Console.WriteLine("Tong= {0}, Hieu= {1}, Tich= {2}, Thuong= {3}.", tong, hieu, tich, thuong);
+        }
+    }
+}
