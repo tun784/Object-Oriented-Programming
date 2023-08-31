377721face4259750e43b7ea58d54d2d7d04e13c
tun784 <thudientu782004@gmail.com>
2023-09-01 00:15:25 +0700

    September 1st

--- Program3.cs
+++ Program3.cs
@@ -1,34 +1,34 @@
 using System;
 
 namespace Bai2{
-    class Program{
-        public class NgayThang{
-            private int day, month, year;
-            public int d{
-                get { return d; }
-                set { day = value }
-            }
-            public int m{
-                get { return m; }
-                set { month = m;}
-            }
-            public int y{
-                get { return y; }
-                set { year = y; }
-            }
-
-            public NgayThang(NgayThang date){
-                day = date.day;
-                month = date.month;
-                year = date.year;
-            }
-
-            public static bool checkLeapYear(){
-                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-                    return true;
-                else return false;
-            }
+    public class NgayThang{
+        private int day, month, year;
+        public int d {
+            get { return d; }
+            set { day = value }
         }
+        public int m {
+            get { return m; }
+            set { month = m;}
+        }
+        public int y {
+            get { return y; }
+            set { year = y; }
+        }
+
+        public NgayThang(NgayThang date){
+            day = date.day;
+            month = date.month;
+            year = date.year;
+        }
+
+        public static bool checkLeapYear(){
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                return true;
+            else return false;
+        }
+    }
+    class Program{
         static void Main(){
             
         }
