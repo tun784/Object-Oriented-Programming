2f6ce3891a14c9ed225b303d6575b027ce2de59c
tun784 <thudientu782004@gmail.com>
2023-09-04 17:37:23 +0700

    September 4th 17:37

---
+++ testAnimal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+// Define a class called Animal
+class Animal {
+  // Declare some private attributes
+  private:
+    string name;
+    int age;
+  // Declare some public methods
+  public:
+    // Constructor that takes name and age as parameters
+    Animal(string n, int a) {
+      name = n;
+      age = a;
+    }
+    // Method that returns the name of the animal
+    string getName() {
+      return name;
+    }
+
+    // Method that returns the age of the animalz
+    int getAge() {
+      return age;
+    }
+
+    // Method that prints a greeting from the animal
+    void greet() {
+      cout << "Hello, I am " << name << " and I am " << age << " years old." << endl;
+    }
+};
+
+// Main function
+int main() {
+  // Create an object of class Animal with name "Lion" and age 5
+  Animal lion("Lion", 5);
+
+  // Call the greet method on the object
+  lion.greet();
+
+  // Print the name and age of the object using the getter methods
+  cout << "The name of the animal is " << lion.getName() << endl;
+  cout << "The age of the animal is " << lion.getAge() << endl;
+
+  return 0;
+}
